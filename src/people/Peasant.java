660c8c9caff4135de1228d4205e7bbe468dcb121
kowkon <sevindikberat@gmail.com>
2017-03-27 20:51:53 +0300

    Peasant class implemented

--- src/people/Peasant.java
+++ src/people/Peasant.java
@@ -1,64 +1,98 @@
 package people;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.RandomAccessFile;
-import java.util.ArrayList;
 import java.util.Random;
-import java.util.Scanner;
 
 public class Peasant {
 
-	private final int nameLength = 20;
+	private final static int firstNameLength = 20;
+	private final static int lastNameLength = 20;
 
 	private String firstName;
 	private String lastName;
-	private char gender;
+	private String gender;
 
 	/**
-	 * Constructor with names
-	 * 
-	 * @param firstName
-	 *            Peasant's first name.
-	 * @param lastName
-	 *            Peasant's last name.
-	 */
-	public Peasant(String firstName, String lastName) {
-		this.gender = chooseGender();
-		this.firstName = firstName;
-		this.lastName = lastName;
-	}
-
-	/**
-	 * Constructor without parameter. This constructor choose first name and
-	 * last name randomly.
+	 * Constructor without parameter. This constructor chooses first name, last
+	 * name and gender randomly.
 	 */
 	public Peasant() {
-		this.firstName = chooseFirstNameMale();
+		this.gender = chooseGender();
+		this.firstName = chooseFirstName(this.gender);
+		this.lastName = chooseLastName();
 	}
 
-	private String chooseFirstNameMale() {
+	/**
+	 * Chooses a random first name according to gender.
+	 * 
+	 * @param gender
+	 *            that defines which file will be opened.
+	 * @return Chosen random first name.
+	 */
+	private String chooseFirstName(String gender) {
+		String result = "";
 		RandomAccessFile file = null;
 		try {
-			file = new RandomAccessFile("firstNamesMale.txt", "r");
+			file = new RandomAccessFile("firstNames" + gender + ".txt", "r");
 			long fileLength = file.length();
-			int seekLength = nameLength + 1;
+			int seekLength = firstNameLength + 1;
 			int rows = (int) (fileLength / seekLength);
 			Random rndm = new Random();
 			file.seek(seekLength * rndm.nextInt(rows));
-			return file.readLine();
+			result = file.readLine();
+			result = removeBlanks(result);
 		} catch (Exception e) {
 			e.printStackTrace();
-			return "";
 		} finally {
 			if (file != null)
 				try {
 					file.close();
-				} catch (IOException e) {
+				} catch (Exception e) {
 					e.printStackTrace();
 				}
 		}
+		return result;
+	}
+
+	/**
+	 * Chooses a random last name.
+	 * 
+	 * @return Chosen random last name.
+	 */
+	private String chooseLastName() {
+		String result = "";
+		RandomAccessFile file = null;
+		try {
+			file = new RandomAccessFile("lastNames.txt", "r");
+			long fileLength = file.length();
+			int seekLength = lastNameLength + 1;
+			int rows = (int) (fileLength / seekLength);
+			Random rndm = new Random();
+			file.seek(seekLength * rndm.nextInt(rows));
+			result = file.readLine();
+			result = removeBlanks(result);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+
+	/**
+	 * Removes blanks from the given string.
+	 * 
+	 * @param s
+	 *            String that blanks will be removed from.
+	 * @return String that has no blank.
+	 */
+	private String removeBlanks(String s) {
+		String result = s;
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) == ' ') {
+				result = s.substring(0, i);
+				break;
+			}
+		}
+		return result;
 	}
 
 	/**
@@ -66,9 +100,9 @@ public class Peasant {
 	 * 
 	 * @return 'm' or 'f'
 	 */
-	private char chooseGender() {
+	private String chooseGender() {
 		Random rndm = new Random();
-		return rndm.nextBoolean() ? 'm' : 'f';
+		return rndm.nextBoolean() ? "Male" : "Female";
 	}
 
 	// GETTERS AND SETTERS
@@ -79,6 +113,10 @@ public class Peasant {
 
 	public String getLastName() {
 		return lastName;
+	}
+
+	public String getGender() {
+		return gender;
 	}
 
 }
