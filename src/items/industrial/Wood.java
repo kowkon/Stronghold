dc730677eab54bed941b944cc6374e98f4fc51b1
kowkon <sevindikberat@gmail.com>
2017-03-27 21:13:33 +0300

    Industrial item class templates created.

---
+++ src/items/industrial/Wood.java
@@ -0,0 +1,20 @@
+package items.industrial;
+
+import castle.Castle;
+import items.Item;
+
+public class Wood extends Item {
+
+	@Override
+	public void invokeBuildingsAdd(Castle castle) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void invokeBuildingsRemove(Castle castle) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
